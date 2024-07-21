a441700fb21a52d11ce0cc26268bccf2c0014263
winnie-ma <xuma8821@gmail.com>
2024-07-22 04:40:56 +1000

    feat: realize transaction list component

---
+++ src/components/Transaction.js
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+export const Transaction = (props) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+  const { transaction } = props;
+  const sign = transaction < 0 ? "-" : "+";
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
