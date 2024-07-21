1b58ee5e4e3fa72bbfc55497f36c26c93313ece5
winnie-ma <xuma8821@gmail.com>
2024-07-22 04:39:45 +1000

    feat: realize income and expense component

---
+++ src/components/IncomeExpense.js
@@ -0,0 +1,39 @@
+import { useContext, useEffect, useState } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+export const IncomeExpense = () => {
+  const { transactions } = useContext(GlobalContext);
+  const [money, setMoney] = useState({
+    income: 0,
+    expense: 0,
+  });
+  useEffect(() => {
+    const amounts = transactions.map((transaction) => transaction.amount);
+    console.log(amounts);
+    const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const expense = amounts
+      .filter((item) => item < 0)
+      .reduce((acc, item) => (acc += -item), 0)
+      .toFixed(2);
+    setMoney({ income, expense });
+  }, [transactions]);
+  return (
+    <div className="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className="money plus">
+          ${money.income}
+        </p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" className="money minus">
+          ${money.expense}
+        </p>
+      </div>
+    </div>
+  );
+};
